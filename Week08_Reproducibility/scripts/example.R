bd4fbb2db6fbdc81eaf6881620158c5468e548b1
Ellen Bledsoe <ebledsoe@arizona.edu>
2025-10-13 15:04:00 -0700

    I can't decide how to structure this repo now that the weeks are different...

---
+++ Week08_Reproducibility/scripts/example.R
@@ -0,0 +1,18 @@
+# This is an R script. 
+
+## What does that mean? ##
+
+# It is a document that only understands R code, so anything I write that isn't 
+# in the language R (such as what I'm writing right now), has to have a # in front
+# of it to be ignored.
+# The file ending for R scripts is .R
+
+## How does this differ from an RMarkdown or Quarto file? ##
+
+# Because RMarkdown combine text with code, we have to specify when the file 
+# should interpret what we are typing as the R language; these are the code chunks.
+# Since this document only understand the R language, we don't need code chunks...
+# and there is no "visual" mode.
+# You can think of this document as basically one giant code chunk!
+
+getwd()
---
+++ Week08_Reproducibility/scripts/weight_function.R
@@ -0,0 +1,6 @@
+# Function for converting from pounds (lb) to kilograms (kg) #
+
+convert_lb_to_kg <- function(weight_lb){
+  kg <- weight_lb * 0.453
+  return(kg)
+}
